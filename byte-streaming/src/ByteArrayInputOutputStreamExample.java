7ce012d8a5f0c2747b9da1e41f9fcfb3253f4e5d
komal <bankarkomal38@gmail.com>
2023-08-10 13:37:56 +0530

    ConsoleInputOutputExample Sample-File.txt ByteArrayInputOutputStreamExample FileInputOutputStreamExample BufferedInputOutputStreamExample Input.txt Output.txt ByteArrayInputOutputStreamExample

--- byte-streaming/src/ByteArrayInputOutputStreamExample.java
+++ byte-streaming/src/ByteArrayInputOutputStreamExample.java
@@ -3,20 +3,37 @@ import java.util.stream.IntStream;
 
 // Input-stream and output stream deal with byte. [1 bit or 8bit]
 // Read/Write or Input/Output
+//InputStream - Consider this as pipe of input data
+//OutputStream - Consider this as pipe of output data
+
+/**
+ * ByteArrayInputStream - this is an implementation of InputStream abstract class.
+ * This class is used to read byte array.
+ * ByteArrayOutputStream - this is an implementation of OutputStream abstract class.
+ * This class is used to write array read from input stream
+ */
 public class ByteArrayInputOutputStreamExample {
     public static void main(String[] args) {
         try{
-            byte[] byteArray = {'H','e','l','l','o','w'};
-            InputStream inputStream = new ByteArrayInputStream(byteArray,0,byteArray.length);
-            // Read
-            OutputStream outputStream = new ByteArrayOutputStream();//Write
+            byte[] byteArray = {'H','e','l','l','o'};
 
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(byteArray,0,byteArray.length);//Read
+            ByteArrayInputStream inputStream1 = new ByteArrayInputStream(byteArray,0,byteArray.length);//Read
+
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();//Write
+            ByteArrayOutputStream outputStream1= new ByteArrayOutputStream();
+
+//            System.out.println("inputStream.readAllBytes() = "+ Arrays.toString(inputStream.readAllBytes()));
             outputStream.write(inputStream.readAllBytes());
 
-            System.out.println(outputStream.toString());
+            byte[] bytesFromOutputStream = outputStream.toByteArray();
 
-        }catch (IOException ioException){
+            System.out.println("outputStream.toString()="+ new String(bytesFromOutputStream, "UTF-8"));
+
+        } catch (IOException ioException){
             ioException.printStackTrace();
         }
     }
+
 }
